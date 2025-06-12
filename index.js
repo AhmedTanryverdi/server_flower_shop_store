a331bf67474007721d0b85066a01c4d350417778
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-06-13 02:01:33 +0300

    Implement product listing endpoint

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 import express from "express";
+import cors from "cors";
+import { getProducts } from "./controllers/productController.js";
 
 const app = express();
+app.use(express.json());
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+	})
+);
+app.use("/catalog", express.static("uploads"));
+
+app.get("/catalog", getProducts);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
